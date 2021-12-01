a6bd40374afe2ea13da615d83f446ae9cd4d1ca0
Akalax <sahakash7512.com>
2021-12-01 18:27:43 +0530

    project completed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 var bodyParser = require('body-parser');
-const mongoose = require('mongoose')
 
 const route = require('./routes/route.js');
 
@@ -9,34 +8,15 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb+srv://user-open-to-all:hiPassword123@cluster0.xgk0k.mongodb.net/<Akash_kumar_sah_Db>?retryWrites=true&w=majority", {useNewUrlParser: true})
+const mongoose = require('mongoose');
+const { response } = require('express');
+
+mongoose.connect("mongodb+srv://user-open-to-all:hiPassword123@cluster0.xgk0k.mongodb.net/bharatesh-database?retryWrites=true&w=majority")
     .then(() => console.log('mongodb running on 27017'))
     .catch(err => console.log(err))
 
-
-//   mongoose.connect("mongodb+srv://user-open-to-all:hiPassword123@cluster0.xgk0k.mongodb.net/Akash_kumar_sah-database?retryWrites=true&w=majority", {useNewUrlParser: true})
-//     .then(() => console.log('mongodb running on 27017'))
-//     .catch(err => console.log(err))
-
-
-// const middleware =function(req,res,next){
-//     const newdate=new Date();
-//     const fullDate=newdate.getDate()+"/"
-//                     + (newdate.getMonth()+1)  + "/" 
-//                     + newdate.getFullYear() + " "  
-//                     + newdate.getHours() + ":"  
-//                     + newdate.getMinutes() + ":" 
-//                     + newdate.getSeconds();
-//       const ip=req.ip;           // Assigning the local ip  in variable ip
-//       const url=req.originalUrl; //Assigning the The end point Or url in variable url
-//       console.log("Date="+fullDate +" "+ "IP =" + ip+ " "+ "URL ="+url);
-//       next();
-
-// }
-// app.use(middleware);    //Declare miiddel
-
 app.use('/', route);
 
-app.listen(process.env.PORT || 3000, function() {
-	console.log('Express app running on port ' + (process.env.PORT || 3000))
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
